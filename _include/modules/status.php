31d7e56bc837d60d4474a85f79f20f4c07a4cb93
L1n3m4st3r <andre.kienzle.ak@gmail.com>
2016-06-28 18:35:46 +0200

    getting the needed port from a array

--- _include/modules/status.php
+++ _include/modules/status.php
@@ -1,14 +1,24 @@
 <?php
 
-// #AwesomeArray for the server adresses
+// Array for the server adresses
 $adresses = array(
     "website" => "Line-Lan.net",
     "database" => "localhost",
-    "mail" => "imappro.zoho.com",
+    "mail" => "mail.line-lan.net",
     "api" => "api.line-lan.net",
     "teamspeak" => "5.230.10.109",
     "dns" => "kevin.ns.cloudflare.com",
     "pingTarget" => "google.de",
+);
+
+// Array with ports of different protocols
+$ports = array(
+   "http" => 80,
+   "https" => 443,
+   "mysql" => 3306,
+   "teamspeak" => 10011,
+   "imap" => 993,
+   "dns" => 53,
 );
 
 // Check the Status of a Webserver
@@ -41,44 +51,44 @@ switch ($q[1]) {
    case NULL:
       //	/api/status/
       echo('Website: ');
-      checkStatus($adresses[website], 80);
+      checkStatus($adresses[website], $ports["https"]);
       echo('<br />');
       echo('Database: ');
-      checkStatus($adresses[database], 3306);
+      checkStatus($adresses[database], $ports["mysql"]);
       echo('<br />');
       echo('Mail: ');
-      checkStatus($adresses[mail], 993);
+      checkStatus($adresses[mail], $ports["imap"]);
       echo('<br />');
       echo('API: ');
-      checkStatus($adresses[api], 80);
+      checkStatus($adresses[api], $ports["https"]);
       echo('<br />');
       echo('Teamspeak: ');
-      checkStatus($adresses[teamspeak], 80);
+      checkStatus($adresses[teamspeak], $ports["teamspeak"]);
       echo('<br />');
       echo('DNS: ');
-      checkStatus($adresses[dns], 53);
+      checkStatus($adresses[dns], $ports["dns"]);
       echo('<br />');
       echo('Ping: ');
       echo (pingStatus($adresses[pingTarget]) . ' ms');
       echo('<br />');
       break;
    case website:
-      checkStatus($adresses[website], 80);
+      checkStatus($adresses[website], $ports["https"]);
       break;
    case database:
-      checkStatus($adresses[database], 3306);
+      checkStatus($adresses[database], $ports["mysql"]);
       break;
    case mail:
-      checkStatus($adresses[mail], 993);
+      checkStatus($adresses[mail], $ports["imap"]);
       break;
    case api:
-      checkStatus($adresses[api], 80);
+      checkStatus($adresses[api], $ports["https"]);
       break;
    case teamspeak:
-      checkStatus($adresses[teamspeak], 80);
+      checkStatus($adresses[teamspeak], $ports["teamspeak"]);
       break;
    case dns:
-      checkStatus($adresses[dns], 53);
+      checkStatus($adresses[dns], $ports["dns"]);
       break;
    case ping:
       echo (pingStatus($adresses[pingTarget]) . ' ms');
